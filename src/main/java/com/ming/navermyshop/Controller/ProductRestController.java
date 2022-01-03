10112839e53557a5f5421af079c78ce3cb191328
min-zi <min-zi@naver.com>
2022-01-04 04:58:15 +0900

    관심상품 등록

--- src/main/java/com/ming/navermyshop/Controller/ProductRestController.java
+++ src/main/java/com/ming/navermyshop/Controller/ProductRestController.java
@@ -2,14 +2,17 @@ package com.ming.navermyshop.Controller;
 
 import com.ming.navermyshop.models.Product;
 import com.ming.navermyshop.models.ProductRepository;
+import com.ming.navermyshop.models.ProductRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
-@RestController // JSON으로 데이터를 주고받음을 선언합니다.
+@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성
+@RestController // JSON으로 데이터를 주고받음을 선언
 public class ProductRestController {
 
     private final ProductRepository productRepository;
@@ -18,5 +21,12 @@ public class ProductRestController {
     @GetMapping("/api/products")
     public List<Product> read() {
         return productRepository.findAll();
+    }
+
+    // 관심상품 등록
+    @PostMapping("/api/products")
+    public Product create(@RequestBody ProductRequestDto requestDto) {
+        Product product = new Product(requestDto);
+        return productRepository.save(product);
     }
 }
